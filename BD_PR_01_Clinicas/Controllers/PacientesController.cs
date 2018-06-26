f977b8bc338930c0bdb477665a050110db3a3d0f
Pedro Vega <pedro1029vega@gmail.com>
2018-06-26 14:05:09 -0600

    para sincronizar

--- BD_PR_01_Clinicas/Controllers/PacientesController.cs
+++ BD_PR_01_Clinicas/Controllers/PacientesController.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BD_PR_01_Clinicas.Models;
 
 namespace BD_PR_01_Clinicas.Controllers
 {
     public class PacientesController : Controller
     {
+        DataClasesDataContext db = new DataClasesDataContext();
         // GET: Pacientes
         public ActionResult Index()
         {
-            return View();
+            List<tbPaciente> lista = (from pac in db.tbPaciente
+                                      join cons in db.tbConsulta on pac.codPaciente equals cons.codPaciente
+                                      where cons.atendido == true
+                                      orderby pac.nombre
+                                      select pac).ToList();
+            return View(lista);
         }
 
         // GET: Pacientes/Details/5
-        public ActionResult Details(int id)
+        public ActionResult HistoriaClinica(int codPaciente)
         {
-            return View();
+            HistoriaClinica historia = new Models.HistoriaClinica();
+            historia.paciente = (from t in db.tbPaciente where t.codPaciente == codPaciente select t).SingleOrDefault();
+            historia.patologicos = (from t in db.tbAntecedentesPatologicos where t.codPaciente == codPaciente select t).SingleOrDefault();
+            historia.noPatologicos = (from t in db.tbAntecedentesNoPatologicos where t.codPaciente == codPaciente select t).SingleOrDefault();
+            historia.desarrollo = (from t in db.tbDesarrollo where t.codPaciente == codPaciente select t).SingleOrDefault();
+            return View(historia);
         }
 
         // GET: Pacientes/Create
