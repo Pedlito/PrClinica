87795dd443ae93b0b1d32a68e894f40e116e8807
Pedro Vega <pedro1029vega@gmail.com>
2018-07-10 22:24:07 -0600

    Agregue Reporte de consultas por fechas

---
+++ BD_PR_01_Clinicas/Controllers/ReportesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BD_PR_01_Clinicas.Models;
+
+namespace BD_PR_01_Clinicas.Controllers
+{
+    public class ReportesController : Controller
+    {
+        DataClasesDataContext db = new DataClasesDataContext();
+        // GET: Reportes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reportes/Details/5
+        public ActionResult ConsultasFechas(string inicio = "", string fin = "")
+        {
+            List<ConsultasFechas> lista = null;
+            if (inicio != "" && fin != "")
+            {
+                lista = (from consulta in db.tbConsulta
+                         join diagnostico in db.tbDiagnostico on consulta.codConsulta equals diagnostico.codConsulta
+                         where diagnostico.fecha.Value.Date >= DateTime.Parse(inicio) & diagnostico.fecha.Value.Date <= DateTime.Parse(fin)
+                         select new ConsultasFechas
+                         {
+                             medico = diagnostico.tbUsuario.nombre,
+                             estudiante = diagnostico.tbUsuario1.nombre,
+                             fecha = diagnostico.fecha.Value,
+                             paciente = consulta.tbPaciente.nombre
+                         }).ToList();
+            }
+            ViewBag.inicio = inicio;
+            ViewBag.fin = fin;
+            return View(lista);
+        }
+
+        // GET: Reportes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reportes/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reportes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reportes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reportes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reportes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BD_PR_01_Clinicas/Models/Reportes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BD_PR_01_Clinicas.Models
+{
+    public class ConsultasFechas
+    {
+        public string paciente { get; set; }
+        public DateTime fecha { get; set; }
+        public string medico { get; set; }
+        public string estudiante { get; set; }
+    }
+}
